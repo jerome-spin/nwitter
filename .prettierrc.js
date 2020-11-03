eb1fd6c76a1bbbad20951ac580e59f2fdce21e6d
Jerome <jerome@geekstudio.co.kr>
2020-11-03 11:02:06 +0900

    Set up the Project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 150,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    htmlWhitespaceSensitivity: 'ignore',
+    spaceBeforeFunctionParen: true,
+};
